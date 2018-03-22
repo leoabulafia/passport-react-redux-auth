d354f80fd2d33e5be65d43645aec7c857a1061ca
leoabulafia <ldavalos.mail@gmail.com>
2018-03-22 02:05:19 +0200

    fix typo in googleClientID

--- services/passport.js
+++ services/passport.js
@@ -27,7 +27,7 @@ passport.deserializeUser((id, done) => {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientId,
+      clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
       proxy: true
